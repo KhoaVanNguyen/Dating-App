3c4fe37cc90861025f48de2e9cdbc593930b4f73
Khoa Nguyen <vankhoa0603@gmail.com>
2017-09-17 07:42:38 +0700

    Save

--- Dating App/LoginVC.swift
+++ Dating App/LoginVC.swift
@@ -19,8 +19,8 @@ class LoginVC: UIViewController {
 
     @IBAction func login_TouchUpInside(_ sender: Any) {
         
-        emailTF.text = "user@gmail.com"
-        passwordTF.text = "123456"
+      //  emailTF.text = "user@gmail.com"
+       // passwordTF.text = "123456"
 
         
 //        emailTF.text = "user2@gmail.com"
